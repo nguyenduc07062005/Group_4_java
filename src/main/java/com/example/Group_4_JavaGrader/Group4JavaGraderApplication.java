991db0b0e333820e9bc1f728a19bc8cf84011fd0
nguyenduc07062005 <nguyenduc07062005@gmail.com>
2026-03-28 19:46:48 +0700

    update project

---
+++ src/main/java/com/example/Group_4_JavaGrader/Group4JavaGraderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Group_4_JavaGrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group4JavaGraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group4JavaGraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Group_4_JavaGrader/Group4JavaGraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Group_4_JavaGrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4JavaGraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
